51d6e38882d6b7668cb483cc1449f4e8d678f48b
s1300023 <136044105+s1300023@users.noreply.github.com>
2024-05-08 13:12:10 +0900

    Update test.py

--- code/test.py
+++ code/test.py
@@ -31,8 +31,10 @@ def create_file(conn:sqlite3.Connection=None, file_name:str='text', new_name:str
 
 def update_file(conn:sqlite3.Connection=None, file_name:str='text', new_name:str='Q1')->None:
     cur = conn.cursor()
-    c.execute("UPDATE files SET new_name = ? WHERE (file_name = ?) or (new_name = ?)", (new_name, original_name, original_name))
+    cur.execute("UPDATE files SET new_name = ? WHERE (file_name = ?)", (new_name, original_name))
     conn.commit()
+    cur.close()
+    return
 
 def main():
     database_path = __file__.replace('code/ex01.py', 'dataset/text_datas.db')
