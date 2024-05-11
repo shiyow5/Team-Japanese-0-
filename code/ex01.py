ac0f762711d3d38bf5b7675addce57f10c4a65eb
s1280126 <87121340+s1280126@users.noreply.github.com>
2024-05-12 02:11:35 +0900

    Update ex01.py

--- code/ex01.py
+++ code/ex01.py
@@ -1,4 +1,63 @@
+import sqlite3
+
+def create_file(conn:sqlite3.Connection=None, file_name:str='text', new_name:str='Q1')->None:
+    cur = conn.cursor()
+    
+    cur.execute(
+        "SELECT * FROM sqlite_master WHERE type='table'"
+    )
+    
+    if (not(cur.fetchall())):
+        cur.execute(
+            "CREATE TABLE files (Original_Name text, New_Name text, Sentence text)"
+        )
+        
+    try:
+        file_path = __file__.replace('code/ex01.py', 'input_files/'+file_name+'.txt')
+        with open(file_path, mode='r', encoding='UTF-8') as file:
+            content = file.read()
+    except FileNotFoundError:
+        print(f"ファイル '{file_path}' が見つかりませんでした。")
+    except Exception as e:
+        print(f"エラーが発生しました: {e}")
+    
+    cur.execute(
+        "INSERT INTO files VALUES (?, ?, ?)", [file_name, new_name, content]
+    )
+    
+    conn.commit()
+    cur.close()
+    return
+
+def update_file(conn:sqlite3.Connection=None, file_name:str='text', new_name:str='Q1')->None:
+    cur = conn.cursor()
+    cur.execute(
+      "UPDATE files SET New_Name = ? WHERE (Original_Name = ?) OR (New_Name = ?)", [new_name, file_name, file_name]
+    )
+    conn.commit()
+    cur.close()
+    return
+  
+def delete_file(conn:sqlite3.Connection=None, file_name:str='text')->None:
+    cur = conn.cursor()
+    cur.execute(
+        "DELETE FROM files WHERE (Original_Name = ?) OR (New_Name = ?)", [file_name, file_name]
+    )
+    conn.commit()
+    cur.close()
+    return
+
 def main():
+    database_path = __file__.replace('code/ex01.py', 'dataset/text_datas.db')
+    
+    conn = sqlite3.connect(database_path)
+    
+    #create_file()テスト用
+    #create_file(conn, 'Author_1', 'K1')
+    #update_file()テスト用
+    #update_file(conn, 'K1', 'Q1')
+    
+    conn.close()
     return
 
 if (__name__ == "__main__"):
