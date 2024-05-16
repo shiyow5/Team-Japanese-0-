e727d55c03b79be1ee577d200076a2e12906daff
shiyow5 <115235232+shiyow5@users.noreply.github.com>
2024-05-16 13:57:39 +0900

    Update ex2_1.py

--- code/ex2_1.py
+++ code/ex2_1.py
@@ -4,7 +4,7 @@ def compare(list1,list2):
 
     for i in range(list1):
         for j in range(list2):
-            if list[i]==list[j]:
+            if list1[i]==list2[j]:
                 x+=1
     
     y=x/len(list1)
