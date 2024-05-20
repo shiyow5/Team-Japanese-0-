0b194f17c9a4ad58235e7d88777d33ef7261fd89
s1300097 <s1300097@u-aizu.ac.jp>
2024-05-20 12:10:32 +0900

    complete frequency()

---
+++ code/ex2_1.py
@@ -0,0 +1,27 @@
+def frequency(text:str = 'text', top_n:int = 0)->list:
+    text = text.split()
+    dict = {}
+    non_wordtoken = ",.;:!?"
+
+    for s in text:
+        if s[-1] in non_wordtoken:
+            s = s[:-1]
+
+        if s in dict:
+            dict[s] += 1
+        else:
+            dict.setdefault(s, 1)
+
+    list = sorted(dict.items(), key = lambda x : x[1], reverse=True)
+    list = [i for i, j in list]
+
+    return list[:top_n]
+
+if __name__ == "__main__":
+    text = "I'm a perfect human.\ntanaka tanaka tanaka!"
+    print(text + "\n")
+
+    word_list = frequency(text, 20)
+    print(word_list)
+    word_list = frequency(text, 3)
+    print(word_list)
