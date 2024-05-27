81439702c2785649c3c3e023f07de31db17788d9
shiyow5 <s1300221@u-aizu.ac.jp>
2024-05-27 16:50:25 +0900

    modify ex2_2.py

--- code/ex2_2.py
+++ code/ex2_2.py
@@ -1,40 +1,24 @@
 import File
-def aaa(Ktextlist):
-    wordlist=[]
-    item=[]
-    listnom=0
-    for i in Ktextlist:
-        a = File.format(i).split()
-        for j in a:
-            x=0
-            for k in range(listnom):
-                if wordlist[k] == j and k<pre:
-                    item[k]+=1
-                    x=1
-            if x == 0:
-                wordlist.append(j)
-                item.append(1)
-                listnom+=1
-        pre=listnom
-    print("all")
-    for i in range(listnom):
-        if item[i] == len(Ktextlist):
-            print(wordlist[i])
-            
-    print("all-1")
-    for i in range(listnom):
-        if item[i] == len(Ktextlist)-1:
-            print(wordlist[i])
-            
-    print("all-2")
-    for i in range(listnom):
-        if item[i] == len(Ktextlist)-2:
-            print(wordlist[i])
+
+def common_word(sentences:list=[], exclude:int=0):
     
-    return
+    word_identify = {}
+    
+    for sentence in sentences:
+        words = File.format(sentence).split()
+        
+        for word in words:
+            if (word not in word_identify.keys()):
+                word_identify[word] = 0
+                
+            word_identify[word] += 1
+            
+    result = [word for word in word_identify.keys() if word_identify[word]>=(len(sentences)-exclude)]
+    
+    return result
 
 text1 = 'bb cc dd ee ff gg'
 text2 = 'bb qq ww rr dd gg'
 text3 = 'rr bb ff gg'
 
-print(aaa([text1, text2, text3]))
+print(common_word([text1, text2, text3]))
