79f27b9c38ae95c76aeae526432f7e545736c305
shiyow5 <s1300221@u-aizu.ac.jp>
2024-06-01 14:31:04 +0900

    modify common_word()

--- code/ex2_2.py
+++ code/ex2_2.py
@@ -5,14 +5,15 @@ def common_word(sentences:list=[], exclude:int=0):
     
     word_identify = {}
     
-    for sentence in sentences:
+    for i, sentence in enumerate(sentences):
         words = NLP.format(sentence).split()
         
         for word in words:
             if (word not in word_identify.keys()):
                 word_identify[word] = 0
                 
-            word_identify[word] += 1
+            if (word_identify[word] == i): # 同じファイル内の単語重複カウント禁止
+                word_identify[word] += 1
             
     result = [word for word in word_identify.keys() if word_identify[word]>=(len(sentences)-exclude)]
     
@@ -21,16 +22,16 @@ def common_word(sentences:list=[], exclude:int=0):
 
 if (__name__ == "__main__"):
     
-    K_files = ['AnwarKhoirul_20', 'AokiToshiaki_4', 'AsanoFumihiko_1', 'ChenJiageng_6', 'CheongKaiYuen_1', 'DangJiannwu_5', 'DefagoXavier_1', 'IkedaKokolo_2', 'InoguchiYasushi_1', 'MatsumotoTadashi_19']
-    for i, K_file in enumerate(K_files):
-        K_files[i] = File.get_sentence(K_file)
+    files = File.get_fileName()
+    for i, file in enumerate(files):
+        files[i] = File.get_sentence(file)
     
-    print(common_word(K_files))
+    print(common_word(files))
 
     """
     text1 = 'bb cc dd ee ff gg'
     text2 = 'bb qq ww rr dd gg'
-    text3 = 'rr bb ff gg'
+    text3 = 'rr bb ff gg rr'
 
     print(common_word([text1, text2, text3]))
     """
